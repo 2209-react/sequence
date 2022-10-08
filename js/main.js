ab52ba4986a27ba1a0391df47186097a2ed500c9
tubi55 <tubi55@nate.com>
2022-10-08 11:37:23 +0900

    마우스 현재 포인터의 좌표값을 200분율로 환산

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 var frame = document.querySelector('section');
-
 var imgs = '';
 
 for (var i = 0; i < 200; i++) {
 	imgs += '<img src="img/pic' + i + '.jpg">';
 }
-
 frame.innerHTML = imgs;
+
+//마우스무브시 브라우저상의 가로축 좌표값을 구하기
+window.addEventListener('mousemove', function (e) {
+	//이백분율 구하는 공식
+	// (현재값/전체값) * 200
+	var wid = window.innerWidth;
+	var percent = parseInt((e.pageX / wid) * 200);
+	console.log(percent);
+});
