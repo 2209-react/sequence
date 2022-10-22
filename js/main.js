4598ed8e17d0dd8c3b791d709f3be929dc8b1a72
tubi55 <tubi55@nate.com>
2022-10-22 09:27:16 +0900

    es6문법으로 변경

--- js/main.js
+++ js/main.js
@@ -1,56 +1,41 @@
-var frame = document.querySelector('section');
-var mask = document.querySelector('aside');
-var imgs = '';
-var delay = convertSpeed('aside');
+const frame = document.querySelector('section');
+const mask = document.querySelector('aside');
+let imgs = '';
 
-//200개의 이미지를 동적으로 생성 후 변수에 할당
-for (var i = 0; i < 200; i++) {
-	imgs += '<img src="img/pic' + i + '.jpg">';
+const convertSpeed = el => {
+	const item = document.querySelector(el);
+	const dur = parseFloat(getComputedStyle(item).transitionDuration) * 1000;
+	return dur;
 }
+const delay = convertSpeed('aside');
+
+for (let i = 0; i < 200; i++) imgs += `<img src="img/pic${i}.jpg">`;
 frame.innerHTML = imgs;
-var imgDOM = frame.querySelectorAll('img');
 
-//소스이미지가 로딩완료될때마다 카운트가 될 변수 추가
-var count = 0;
+const imgDOM = frame.querySelectorAll('img');
+let count = 0;
 
-//img dom에 적용된 소스이미지가 로딩완료될때마다 반복
-imgDOM.forEach(function (img) {
-	img.onload = function () {
+imgDOM.forEach(img => {
+	img.onload = () => {
 		count++;
 
-		//증가되는 카운트값을 다시 백분율로 변환
-		var percent = parseInt((count / 200) * 100);
+		const percent = parseInt((count / 200) * 100);
 		mask.querySelector('span').innerText = percent;
 		mask.querySelector('.bar').style.width = percent + '%';
 
-		//백분율이 100에 도달하면
 		if (percent === 100) {
-			//마스크에 클래스off를 붙여서 사라지는 모션적용
 			mask.classList.add('off');
-
-			//해당 마스크의 transition시간인 2초 뒤에
-			setTimeout(function () {
-				//마스크 dom을 아예 제거
-				mask.remove();
-			}, delay);
+			setTimeout(() => mask.remove(), delay);
 		}
 	};
 });
 
-//마우스 무브시 마우스 포인터의 위치 순번의 이미지만 활성화
-window.addEventListener('mousemove', function (e) {
-	var wid = window.innerWidth;
-	var percent = parseInt((e.pageX / wid) * 200);
-
-	imgDOM.forEach(function (el) {
-		el.style.display = 'none';
-	});
+window.addEventListener('mousemove', e => {
+	const wid = window.innerWidth;
+	const percent = parseInt((e.pageX / wid) * 200);
+	imgDOM.forEach(el => el.style.display = 'none');
 	imgDOM[percent].style.display = 'block';
 });
 
-//특정 DOM의 transition-duration값을 밀리세컨드로 반환하는 함수
-function convertSpeed(el) {
-	var item = document.querySelector(el);
-	var dur = parseFloat(getComputedStyle(item).transitionDuration) * 1000;
-	return dur;
-}
+
+
